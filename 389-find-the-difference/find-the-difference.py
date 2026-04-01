d8188225c54ce7f871b50b7bc7c2ea0887e59455
kaladhar bandari <150433155+kaladharb@users.noreply.github.com>
2026-04-01 18:39:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (78.71%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        res=0
+
+        for ch in s:
+            res^=ord(ch)
+        for ch in t:
+            res^=ord(ch)
+        res=chr(res)
+        return res
