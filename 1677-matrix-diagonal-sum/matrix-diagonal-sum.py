338af7aa70c9171ac667ca37e82ac9088dd4f4e3
kaladhar bandari <150433155+kaladharb@users.noreply.github.com>
2026-05-26 23:14:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (16.33%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -3,12 +3,10 @@ class Solution:
         sumi=0
         n=len(mat)
         for i in range(n):
-            for j in range(n):
-                if i==j:
-                    sumi+=mat[i][j]
-                if i+j==n-1:
-                    sumi+=mat[i][j]
-        if n%2!=0:   
-            sumi=sumi-(mat[n//2][n//2])
+            sumi+=mat[i][i]
+            sumi+=mat[i][n-i-1]
+        
+        if n%2!=0:
+            sumi-=mat[n//2][n//2]
         return sumi
         
