17bb1013162ba4919d775556fdfdb0a4fc155aff
kaladhar bandari <150433155+kaladharb@users.noreply.github.com>
2026-08-18 19:00:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (52.52%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -0,0 +1,39 @@
+class MyStack:
+
+    def __init__(self):
+        self.q1=deque()
+        self.q2=deque()
+
+    def push(self, x: int) -> None:
+
+        while self.q1:
+            self.q2.append(self.q1.popleft())
+        
+        self.q1.append(x)
+
+        while self.q2:
+            self.q1.append(self.q2.popleft())
+        
+
+    def pop(self) -> int:
+       return self.q1.popleft()
+        
+
+    def top(self) -> int:
+        return self.q1[0]
+        
+
+    def empty(self) -> bool:
+        if len(self.q1)==0:
+            return True
+        else:
+            return False
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
